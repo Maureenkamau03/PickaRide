b0e4373c1b7f097e241bbc9b11683b3e0c793453
McRonaah <ronaldcheruiyot342@gmail.com>
2022-11-09 12:04:45 +0300

    updated icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,32 +10,35 @@ function Footer() {
                 </div>
             <div className="footer">
                 <div className="icon">
+                    <h2>Our company</h2>
+                    <p>Follow us on</p>
                     <div className="icons">
                     <a href="https://www.facebook.com/ronald.beloved/" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/1312/1312139.png" alt=""/>
+                        <img src="https://cdn-icons-png.flaticon.com/128/4401/4401395.png" alt=""/>
                     </a>
                     <a href="https://twitter.com/RonaahB" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/3670/3670151.png" alt=""/>
+                        <img src="https://cdn-icons-png.flaticon.com/128/2504/2504839.png" alt=""/>
                     </a>
-                    <a href="https://www.linkedin.com/in/ronald-kipchirchir-034983246/" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/145/145807.png" alt=""/>
-                    </a>
-                    </div>
-                    <div className="icons">
-                    <a  href="/" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/4391/4391603.png" alt=""/>
-                    </a>
-                    <a href="" rel="noopener">
+                    <a href="insta" rel="noopener">
                         <img src="https://cdn-icons-png.flaticon.com/128/4138/4138124.png" alt=""/>
                     </a>
                     <a href="https://www.youtube.com/channel/UCoO82bia4WfA19iLEVUX2iw" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/3938/3938026.png" alt=""/>
+                        <img src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png" alt=""/>
                     </a>
                     </div>
                 </div>
+                <div>
+                    <h2>Products</h2>
+                </div>
+                <div>
+                    <h2>customers</h2>
+                </div>
+                <div>
+                    <h2>terms and conditions</h2>
+                </div>
                 <div className="copyright">
                     <p>All rights reserved &copy;</p>
-                    <p>CopyRight By Car Dealers Moringa School</p>
+                    <p>CopyRight By Pickaride</p>
                 </div>
             </div>
         </footer>
