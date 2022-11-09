d5ad3643664abadb8d265e2a95ddfdedf068671c
McRonaah <ronaldcheruiyot342@gmail.com>
2022-11-09 12:21:52 +0300

    terms and conditions

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <div className="line">
                     <hr/>
                     <hr/>
-                </div>
+            </div>
             <div className="footer">
                 <div className="icon">
                     <h2>Our company</h2>
@@ -16,11 +16,11 @@ function Footer() {
                     <a href="https://www.facebook.com/ronald.beloved/" rel="noopener">
                         <img src="https://cdn-icons-png.flaticon.com/128/4401/4401395.png" alt=""/>
                     </a>
-                    <a href="https://twitter.com/RonaahB" rel="noopener">
-                        <img src="https://cdn-icons-png.flaticon.com/128/2504/2504839.png" alt=""/>
-                    </a>
                     <a href="insta" rel="noopener">
                         <img src="https://cdn-icons-png.flaticon.com/128/4138/4138124.png" alt=""/>
+                    </a>
+                    <a href="https://twitter.com/RonaahB" rel="noopener">
+                        <img src="https://cdn-icons-png.flaticon.com/128/2504/2504839.png" alt=""/>
                     </a>
                     <a href="https://www.youtube.com/channel/UCoO82bia4WfA19iLEVUX2iw" rel="noopener">
                         <img src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png" alt=""/>
@@ -32,14 +32,17 @@ function Footer() {
                 </div>
                 <div>
                     <h2>customers</h2>
-                </div>
-                <div>
-                    <h2>terms and conditions</h2>
+                    <p>customer support</p>
+                    <p>after sale services</p>
                 </div>
                 <div className="copyright">
+                <h2>terms and conditions</h2>
+                    <p>Privacy policy </p>
                     <p>All rights reserved &copy;</p>
                     <p>CopyRight By Pickaride</p>
                 </div>
+               
+                
             </div>
         </footer>
     );
