4373a91210cacb4762b600992d502ad9509a703c
janekigenyi <janekigenyi@gmail.com>
2022-11-10 15:52:07 +0300

    created a new branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./App.css";
 
-function App() {
-	return (
-		<div>
-			<h1>Hello world</h1>
-		</div>
-	);
+
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
 }
 
 export default App;
+
